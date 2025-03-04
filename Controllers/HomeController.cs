3f364be5f20d84de8f03aa99f11defc2a7a9a881
igl-myrick <igl.myrick@gmail.com>
2025-03-04 14:40:52 -0800

    add Home controller and Index route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcApiCall.Models;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace MvcApiCall.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly string _apikey;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _apikey = configuration["NYT"];
+    }
+
+    public IActionResult Index()
+    {
+        List<Article> allArticles = Article.GetArticles(_apikey);
+        return View(allArticles);
+    }
+  }
+}
